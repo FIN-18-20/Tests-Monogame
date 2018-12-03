1efc6d10a3256ae1922e43620f02e6010f50f836
Kalaneee <kalanehd@gmail.com>
2018-12-03 16:24:30 +0100

    Finished first tutorial

---
+++ Valentin/Tutorials/1-CreatingAnApplication/Game1/Game1.cs
@@ -0,0 +1,127 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+
+namespace Game1
+{
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        Texture2D texture;
+        Vector2 position;
+        
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+
+            //this.Activated += (sender, args) => { this.Window.Title = "Active Application";  };
+            //this.Deactivated += (sender, args) => { this.Window.Title = "UnActive Application"; };
+
+            this.IsFixedTimeStep = true; // default value
+            this.TargetElapsedTime = new TimeSpan(0, 0, 0, 0, 33); // about 33 fps : call update 30 times per sec and tries to call draw 30 times per sec
+            //this.graphics.SynchronizeWithVerticalRetrace = false; // vsync
+        }
+
+        protected override void OnActivated(object sender, EventArgs args)
+        {
+            Window.Title = "Active Application";
+            base.OnActivated(sender, args);
+        }
+
+        protected override void OnDeactivated(object sender, EventArgs args)
+        {
+            Window.Title = "UnActive Application";
+            base.OnDeactivated(sender, args);
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+            position = new Vector2(0, 0);
+            texture = new Texture2D(this.GraphicsDevice, 100, 100);
+            Color[] colorData = new Color[100 * 100];
+            for (int i = 0; i < 10000; i++)
+            {
+                colorData[i] = Color.Red;
+            }
+            texture.SetData<Color>(colorData);
+
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+
+            // TODO: use this.Content to load your game content here
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// game-specific content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+            texture.Dispose();
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            if (IsActive) // pause the game if it's not focus
+            {
+                if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                    Exit();
+
+                // TODO: Add your update logic here
+                position.X += 60f * (float)gameTime.ElapsedGameTime.TotalSeconds; // 60px per sec
+                if (position.X > this.GraphicsDevice.Viewport.Width)
+                    position.X = 0;
+
+                //gameTime.IsRunningSlowly = true if can't reach TargetElapsedTime speed (here 30 fps)
+                base.Update(gameTime);
+            }
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+            var fps = 1 / gameTime.ElapsedGameTime.TotalSeconds;
+            Window.Title = fps.ToString();
+            spriteBatch.Begin();
+            spriteBatch.Draw(texture, position);
+            spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
