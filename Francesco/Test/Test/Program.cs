70e7e6c8bf74716b4a1182445977f65effd81dec
Lothindir <lothindir@gmail.com>
2019-01-21 22:09:33 +0100

    Test project - nothing relevant here

---
+++ Francesco/Test/Test/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<uint,uint?> dict = new Dictionary<uint, uint?>();
+
+            uint idCount = 0;
+            bool added = false;
+
+            for (uint i = 0; i < 10; i++)
+            {
+                dict.Add(i,i);
+                idCount++;
+            }
+
+            dict[5] = null;
+            idCount--;
+
+            foreach (var entry in dict)
+            {
+                Console.WriteLine(entry.Key + " : " + entry.Value);
+            }
+
+            for (uint i = 0; i < dict.Count; i++)
+            {
+                if (dict[i].Equals(null))
+                {
+                    dict[i] = i;
+                    added = true;
+                }
+            }
+
+            if (!added)
+            {
+                dict.Add(idCount,idCount);
+            }
+
+            foreach (var entry in dict)
+            {
+                Console.WriteLine(entry.Key + " : " + entry.Value);
+            }
+        }
+    }
+}
